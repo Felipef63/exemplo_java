e2c3e229d413ccbd0b562d5a2f35d6e091633022
felipef63 <antoniaclaudete2012@hotmail.com>
2019-06-25 12:34:44 -0300

    exemplo2

---
+++ src/aula/exemplo2.java
@@ -0,0 +1,17 @@
+package aula;
+
+import javax.swing.JOptionPane;
+
+public class exemplo2 {
+	
+	public static void main(String[] args) {
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+		double d = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+		
+		// TODO Auto-generated method stub
+		System.out.println("i = " + i);
+		System.out.println("d = " + d);			
+	}
+	
+
+}
